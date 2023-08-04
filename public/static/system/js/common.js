9eb00ad324ab588141b0390a2a83af53ec13a90d
Ying <coolsec@foxmail.com>
2023-08-04 11:08:44 +0800

    fix: 修复layui样式，增加show.xx函数

--- public/static/system/js/common.js
+++ public/static/system/js/common.js
@@ -2,7 +2,7 @@
 
 // 以下代码是配置layui扩展模块的目录，每个页面都需要引入
 layui.config({
-    version: 'v1.1.8',
+    version: 'v1.1.9',
     base: getProjectUrl() + 'module/'
 }).extend({
     tags: 'tags/tags',
@@ -19,26 +19,72 @@ layui.config({
     tableMerge: 'soulTable/tableMerge',
     tableFilter: 'soulTable/tableFilter',
     excel: 'soulTable/excel',
-}).use(['admin','jquery'], function () {
-    var $ = layui.jquery;       // jquery的
-    var admin = layui.admin;    // amdin模块的
+}).use(['admin', 'jquery', 'show'], function () {
+    let $ = layui.jquery;       // jquery的
+    let admin = layui.admin;    // amdin模块的
 });
 
 // 获取当前项目的根路径，通过获取layui.js全路径截取assets之前的地址
 function getProjectUrl() {
 
-    var layuiDir = layui.cache.dir;
+    let layuiDir = layui.cache.dir;
     if (!layuiDir) {
-        var js = document.scripts, last = js.length - 1, src;
-        for (var i = last; i > 0; i--) {
+        let js = document.scripts, last = js.length - 1, src;
+        for (let i = last; i > 0; i--) {
             if (js[i].readyState === 'interactive') {
                 src = js[i].src;
                 break;
             }
         }
-        var jsPath = src || js[last].src;
+        let jsPath = src || js[last].src;
         layuiDir = jsPath.substring(0, jsPath.lastIndexOf('/') + 1);
     }
 
     return layuiDir.substring(0, layuiDir.indexOf('layui'));
 }
+
+layui.define(['notice'], function (exports) {
+    "use strict";
+
+    let MODULE_SHOW_NAME = {};
+    let notice = layui.notice, MOD_NAME = 'show';
+
+    // 正常消息通知
+    MODULE_SHOW_NAME.msg = function (msg) {
+        notice.success({
+            message: msg,
+        });
+    }
+
+    // 错误消息通知
+    MODULE_SHOW_NAME.error = function (msg) {
+        notice.error({
+            message: msg,
+        });
+    }
+
+    // 警告消息通知
+    MODULE_SHOW_NAME.warning = function (msg) {
+        notice.warning({
+            message: msg,
+        });
+    }
+
+    // 信息消息通知
+    MODULE_SHOW_NAME.info = function (msg) {
+        notice.info({
+            message: msg,
+        });
+    }
+
+    // 消息通知
+    MODULE_SHOW_NAME.notice = function (title, msg, options = {}) {
+        notice.show({
+            title: title,
+            message: msg,
+            ...options
+        });
+    }
+
+    exports(MOD_NAME, MODULE_SHOW_NAME);
+})
